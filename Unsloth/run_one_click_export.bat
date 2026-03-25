f4d1536f345110f0ac7720c4b961f75817a201bc
AzusaFish <legends_fish@outlook.com>
2026-03-25 14:25:57 +0800

    V1.0.1

--- Unsloth/run_one_click_export.bat
+++ Unsloth/run_one_click_export.bat
@@ -4,13 +4,11 @@ chcp 65001 >nul
 
 cd /d "%~dp0"
 
-if "%HF_TOKEN%"=="" (
-  set HF_TOKEN=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
-)
-
 set HTTP_PROXY=http://127.0.0.1:7897
 set HTTPS_PROXY=http://127.0.0.1:7897
 set ALL_PROXY=http://127.0.0.1:7897
+
+set HF_TOKEN=your_huggingface_token_here
 
 "%~dp0.conda\python.exe" "%~dp0one_click_export.py" --token "%HF_TOKEN%"
 
